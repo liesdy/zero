6b7b4ce6a0f442aa0bd86a7f05c67c3a105fc6db
洌水剑 <411476852@qq.com>
2020-09-30 17:35:20 +0800

    thunkify提交

--- src/components/next/thunkify.vue
+++ src/components/next/thunkify.vue
@@ -60,6 +60,7 @@ export default {
         for (var i = 0; i < args.length; ++i) {
           args[i] = arguments[i];
         }
+        console.log("args0: ", args);
         return function(done) {
           var called;
 
@@ -68,7 +69,7 @@ export default {
             called = true;
             done.apply(null, arguments);
           });
-
+          console.log("args1: ", args);
           try {
             fn.apply(ctx, args);
           } catch (err) {
